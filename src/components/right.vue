5a323dfaa98bf827715f210b937c3031d8856e3f
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-09-17 17:39:57 +0200

    Pass props to fill out the middle and right section header

--- src/components/right.vue
+++ src/components/right.vue
@@ -1,4 +1,10 @@
 <template>
-  <div class="flex-1">
+  <div class="flex flex-1 flex-col items-center">
+    <Header class="mt-15" title="Boost" :icon="charIcon" />
   </div>
 </template>
+
+<script setup>
+const charIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-rocket-icon lucide-rocket"><path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"/><path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z"/><path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0"/><path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5"/></svg>`;
+import Header from "./Header.vue";
+</script>
