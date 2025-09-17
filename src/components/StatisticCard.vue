faf3029089d73777bee50d2dee5631f9e06bbaac
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-09-17 18:06:29 +0200

    Create a statistic card component to showcase game details

---
+++ src/components/StatisticCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="border-1 border-amber-900 px-10 py-5 rounded-xl">
+    <div class="flex flex-col gap-2">
+      <h1>{{ title }}</h1>
+      <div class="flex flex-row gap-2.5">
+        <div v-html="icon"></div>
+        <h1>+ 531</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+});
+</script>
--- src/components/left.vue
+++ src/components/left.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="flex flex-1 flex-col items-center">
     <Header class="mt-15" title="Statistics" :icon="charIcon"/>
-
+    <StatisticCard class="mt-30" title="Average Per Minute" :icon="cookieIcon"/>
   </div>
 </template>
 
 <script setup>
 import Header from './Header.vue';
+import StatisticCard from './StatisticCard.vue';
 const charIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chart-no-axes-combined-icon lucide-chart-no-axes-combined"><path d="M12 16v5"/><path d="M16 14v7"/><path d="M20 10v11"/><path d="m22 3-8.646 8.646a.5.5 0 0 1-.708 0L9.354 8.354a.5.5 0 0 0-.707 0L2 15"/><path d="M4 18v3"/><path d="M8 14v7"/></svg>`
+const cookieIcon = `<svg xmlns"http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cookie-icon lucide-cookie"><path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5"/><path d="M8.5 8.5v.01"/><path d="M16 15.5v.01"/><path d="M12 12v.01"/><path d="M11 17v.01"/><path d="M7 14v.01"/></svg>`;
 </script>
