4c5e3d186fe7534356483321f9fa7f21386e7ee7
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-09-17 18:10:54 +0200

    Add some styles to the first statistic card

--- src/components/StatisticCard.vue
+++ src/components/StatisticCard.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="border-1 border-amber-900 px-10 py-5 rounded-xl">
+  <div class="bg-[#4e3518] px-10 py-5 rounded-xl text-white">
     <div class="flex flex-col gap-2">
-      <h1>{{ title }}</h1>
+      <h1 class="font-bold">{{ title }}</h1>
       <div class="flex flex-row gap-2.5">
         <div v-html="icon"></div>
-        <h1>+ 531</h1>
+        <h1 class="font-light">+ 531</h1>
       </div>
     </div>
   </div>
