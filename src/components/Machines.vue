2d2d680e423533fb387801b3ae8faf54d14de46c
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-09-23 21:12:30 +0200

    Create machines card for the shop

---
+++ src/components/Machines.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-row">
+    <div>
+      <p>image</p>
+    </div>
+    <div class="flex flex-col">
+      <h2>{{ title }}</h2>
+      <div class="flex flex-row">
+        <h3>+{{ bonus }}CPS</h3>
+        <div v-html="cookieIcon"></div>
+        <h3>{{ cost }}</h3>
+      </div>
+    </div>
+    <div>
+      <h1>{{ possession }}</h1>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const title = ref("");
+const bonus = ref(0);
+const cost = ref(0);
+const possession = ref(0);
+const cookieIcon = `<svg xmlns"http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cookie-icon lucide-cookie"><path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5"/><path d="M8.5 8.5v.01"/><path d="M16 15.5v.01"/><path d="M12 12v.01"/><path d="M11 17v.01"/><path d="M7 14v.01"/></svg>`;
+</script>
