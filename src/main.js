c3d82a68e67a5bd8324df47f5e090b6431b72da2
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-09-24 14:21:45 +0200

    Install and create a store with pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import "./main.css"
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./main.css";
+import { createPinia } from "pinia";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(createPinia());
+app.mount("#app");
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+export const useStore = defineStore("main", () => {
+  const cookieProduced = ref(0);
+});
