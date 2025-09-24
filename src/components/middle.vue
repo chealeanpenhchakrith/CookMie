4328de59b3659a82b1b194cfc4fed5cefee19553
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-09-24 14:33:11 +0200

    Define the global cookieProduced variable in a pinia store

--- src/components/middle.vue
+++ src/components/middle.vue
@@ -3,7 +3,7 @@
     <Header class="mt-15" title="Cookie Industry" :icon="charIcon" />
     <div class="flex flex-row gap-2.5 mt-25">
       <div v-html="cookieIcon"></div>
-      <h1>{{ score }}</h1>
+      <h1>{{ store.cookieProduced }}</h1>
     </div>
     <div class="mt-10">
       <img
@@ -18,13 +18,16 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-const charIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cookie-icon lucide-cookie"><path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5"/><path d="M8.5 8.5v.01"/><path d="M16 15.5v.01"/><path d="M12 12v.01"/><path d="M11 17v.01"/><path d="M7 14v.01"/></svg>`;
+import { useStore } from "@/store";
 import Header from "./Header.vue";
-const cookieIcon = `<svg xmlns"http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cookie-icon lucide-cookie"><path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5"/><path d="M8.5 8.5v.01"/><path d="M16 15.5v.01"/><path d="M12 12v.01"/><path d="M11 17v.01"/><path d="M7 14v.01"/></svg>`;
-const score = ref(0);
+
+const store = useStore();
 
 function incrementCookie() {
-  score.value += 1;
+  store.cookieProduced += 1;
 }
+
+const charIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cookie-icon lucide-cookie"><path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5"/><path d="M8.5 8.5v.01"/><path d="M16 15.5v.01"/><path d="M12 12v.01"/><path d="M11 17v.01"/><path d="M7 14v.01"/></svg>`;
+
+const cookieIcon = `<svg xmlns"http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cookie-icon lucide-cookie"><path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5"/><path d="M8.5 8.5v.01"/><path d="M16 15.5v.01"/><path d="M12 12v.01"/><path d="M11 17v.01"/><path d="M7 14v.01"/></svg>`;
 </script>
--- src/store.js
+++ src/store.js
@@ -2,4 +2,5 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 export const useStore = defineStore("main", () => {
   const cookieProduced = ref(0);
+  return { cookieProduced };
 });
