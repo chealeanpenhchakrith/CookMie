cba5550e8b59aed83bb8a4d7ee4b0bb763040b93
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-09-24 21:44:34 +0200

    Update the cookie currency based on the multiplier cost

--- src/store.js
+++ src/store.js
@@ -4,6 +4,16 @@ export const useStore = defineStore("main", () => {
   const cookieProduced = ref(0);
   const multiplierBonus = ref(1);
   function cookieMultiplier(multiplier) {
+    switch (multiplier) {
+      case 2:
+        cookieProduced.value -= 100;
+      case 3:
+        cookieProduced.value -= 500;
+      case 5:
+        cookieProduced.value -= 2000;
+      case 10:
+        cookieProduced.value -= 10000;
+    }
     multiplierBonus.value = multiplier;
   }
   function cookieIncrement() {
