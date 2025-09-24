cf99ad4af04d4ede49840719a64940c6c5e044ce
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-09-24 22:55:33 +0200

    Fix the switch case with break statement forgotten

--- src/store.js
+++ src/store.js
@@ -1,20 +1,24 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 export const useStore = defineStore("main", () => {
-  const cookieProduced = ref(0);
+  const cookieProduced = ref(10000);
   const multiplierBonus = ref(1);
   function cookieMultiplier(multiplier) {
+    multiplierBonus.value = multiplier;
     switch (multiplier) {
       case 2:
         cookieProduced.value -= 100;
+        break;
       case 3:
         cookieProduced.value -= 500;
+        break;
       case 5:
         cookieProduced.value -= 2000;
+        break;
       case 10:
         cookieProduced.value -= 10000;
+        break;
     }
-    multiplierBonus.value = multiplier;
   }
   function cookieIncrement() {
     cookieProduced.value += 1 * multiplierBonus.value;
