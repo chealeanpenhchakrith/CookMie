1424659e60022b6f5495598596a9d4b9249c3f44
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-09-29 00:43:27 +0200

    Last Attempt fixing left component import issue

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
   <div
     class="flex flex-row h-dvh text-[#632e13] divide-x-5 divide-line bg-[#f8efd7]"
   >
-    <left />
+    <Left />
     <Middle />
     <Right />
   </div>
 </template>
 
 <script setup>
-import left from "./components/Left.vue";
+import Left from "./components/Left.vue";
 import Right from "./components/Right.vue";
 import Middle from "./components/Middle.vue";
 </script>
